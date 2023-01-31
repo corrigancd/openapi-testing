67ff94e1d92b3057229c8f6784345576430b34bc
Kenny Wilde <kenny.wilde@siren.io>
2023-01-31 12:50:57 +0000

    hapi implementation

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 // src/server.ts
 import { app } from "./app";
 
-const port = process.env.PORT || 3000;
+const port = Number(process.env.PORT) || 3000;
 
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+app(port).start();
+
+console.log(`Example app listening at http://localhost:${port}`);
