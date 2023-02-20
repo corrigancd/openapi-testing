7719bcf0f6a9fd933bf59309509c2cac9afecf71
phHank <philip.hankey@siren.io>
2023-02-20 14:03:10 +0000

    Use Keyword type and add JSON comments

--- src/routes/saved_objects/saved_object_types/dashboard.ts
+++ src/routes/saved_objects/saved_object_types/dashboard.ts
@@ -1,36 +1,34 @@
-import { Range } from "../types";
+import { Keyword, Range, SavedObjectVersions } from "../types";
 
 interface RefreshInterval {
   display: string;
   pause: boolean;
-  value: number
+  value: number;
+  section?: number;
 }
-
-type DashboardType = 'dashboard'
 
 export type Dashboard_13_2_0 = {
   attributes: {
     title: string;
+    type?: Keyword;
     description?: string;
     hits?: Range<0, 999>;
-    columns?: string;
-    sort?: string;
-    panelsJSON: string;
-    optionsJSON: string;
-    uiStateJSON: string;
-    coatJSON: string;
-    flexLayoutJSON?: string;
-    timeRestore: boolean;
-    timeMode?: string;
-    timeTo?: string;
-    timeFrom?: string;
-    refreshInterval?: RefreshInterval;
-    scriptIds: string[];
-    version: number;
+    coatJSON: string; //JSONValue
     kibanaSavedObjectMeta: {
-      searchSourceJSON: string;
+      searchSourceJSON: string; //JSONValue
     };
+    panelsJSON: string; //JSONArray
+    optionsJSON: string; //JSONValue
+    uiStateJSON: string; //JSONValue
+    flexLayoutJSON?: string; //JSONValue
+    timeRestore: boolean;
+    timeMode?: Keyword;
+    timeFrom?: Keyword;
+    timeTo?: Keyword;
+    refreshInterval?: RefreshInterval;
+    scriptIds: Keyword[];
+    version: SavedObjectVersions;
   };
-  type: DashboardType;
-  version: number | string;
+  version?: number | string;
+  iGlobal: boolean;
 }
